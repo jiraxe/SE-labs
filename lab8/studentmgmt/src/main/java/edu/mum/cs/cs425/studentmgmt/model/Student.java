0f9ffafb72d6c08cae0ef67d1440e57c796ee49e
jiraxe <ez>
2020-03-06 00:52:48 -0600

    lab 8 done

---
+++ lab8/studentmgmt/src/main/java/edu/mum/cs/cs425/studentmgmt/model/Student.java
@@ -0,0 +1,133 @@
+package edu.mum.cs.cs425.studentmgmt.model;
+
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+    @NotNull
+    @Column(name="studentNumber")
+    private String studentNumber;
+    @NotNull
+    @Column(name="firstName")
+    private String firstName;
+    @Column(name="middleName")
+    private String middleName;
+    @NotNull
+    @Column(name="lastName")
+    private String lastName;
+    @Column(name="cgpa")
+    private double cgpa;
+    @Column(name="dateOfEnrollment")
+    private LocalDate dateOfEnrollment;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Transcript transcript;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "classroomId", nullable = false)
+    private Classroom classroom;
+    public Student() {
+    }
+
+    public Transcript getTranscript() {
+        return transcript;
+    }
+
+    public void setTranscript(Transcript transcript) {
+        this.transcript = transcript;
+    }
+
+    public Classroom getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+    }
+
+    public Student(String studentNumber, String firstName, String middleName, String lastName, double cgpa, LocalDate dateOfEnrollment) {
+        this.studentNumber = studentNumber;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.cgpa = cgpa;
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public LocalDate getDateOfEnrollment() {
+        return dateOfEnrollment;
+    }
+
+    public void setDateOfEnrollment(LocalDate dateOfEnrollment) {
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", studentNumber='" + studentNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", cgpa=" + cgpa +
+                ", dateOfEnrollment=" + dateOfEnrollment +
+                '}';
+    }
+}
